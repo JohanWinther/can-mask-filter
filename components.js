eed3dbcd14345aa1901f8c23de995b5ed2fb97f8
Johan Winther <johan.winther@aptiv.com>
2020-01-31 14:14:10 +0100

    Adds generalised html components

--- components.js
+++ components.js
@@ -1,26 +1,136 @@
 class HexInputList extends HTMLOListElement {
     constructor() {
         super();
-
-        this.style.setProperty("list-style", "none");
-        let li = document.createElement("li");
-        let hexInput = document.createElement(HexInput);
-        hexInput.setAttribute("is", "hex-input");
-        hexInput.classList.add("input", "is-size-1");
-        li.appendChild(hexInput);
-        this.appendChild(li);
-        console.log(li);
     }
+
+    connectedCallback() {
+        // Input event
+        this.addEventListener("input", (e) => {
+            if (!e.data) {
+                e.target.value = "0";
+                let prev = e.target.parentElement.previousElementSibling;
+                if (prev && !prev.firstElementChild.disabled) prev.firstElementChild.focus();
+            } else {
+                let reData = e.data.match(/[0-9a-fA-F]/);
+                if (reData) {
+                    e.target.value = reData[0].toUpperCase();
+                } else {
+                    let prevVal = e.target.value.match(/[0-9a-fA-F]/);
+                    if (prevVal) {
+                        e.target.value = prevVal[0].toUpperCase();
+                    } else {
+                        e.target.value = "0";
+                    }
+                }
+                let next = e.target.parentElement.nextElementSibling;
+                if (next) next.firstElementChild.focus();
+            }
+            document.querySelectorAll("[is=id-table]").forEach(el => el.highlightIds());
+        });
+
+        // Move caret event
+        this.addEventListener("keydown", (e) => {
+            if (e.keyCode == 37 || e.keyCode == 39) {
+                let directionIsRight = (e.keyCode - 37)/2;
+                if (directionIsRight) {
+                    let next = e.target.parentElement.nextElementSibling;
+                    if (next) next.firstElementChild.focus();
+                } else {
+                    let prev = e.target.parentElement.previousElementSibling;
+                    if (prev && !prev.firstElementChild.disabled) prev.firstElementChild.focus();
+                }
+            }
+        });
+    }
+
+
+    static get observedAttributes() { return ["data-count"]; }
+
+    attributeChangedCallback(name, oldValue, newValue) {
+        if (name !== "data-count") return;
+
+        while (this.firstElementChild) {
+            this.removeChild(this.firstElementChild);
+        }
+
+        // Add inputs
+        const count = parseInt(this.dataset.count);
+        for (let i = 0; i < count + 2; i++) {
+            let inputElement = document.createElement("input");
+            inputElement.type = "text";
+            inputElement.maxLength = 2;
+            inputElement.value = "0";
+            inputElement.classList.add("input");
+            if (i < 2) {
+                inputElement.disabled = true;
+                inputElement.value = "0x".substr(i, 1);
+            }
+            let liElement = document.createElement("li");
+            liElement.appendChild(inputElement);
+            this.appendChild(liElement);
+        }
+        document.querySelectorAll("[is=id-table]").forEach(el => el.highlightIds());
+    }
+
+    getHexValue() {
+        return parseInt([...this.children].slice(2).map(e => e.firstElementChild.value).join(""), 16);
+    }
+
+
 }
 
-class HexInput extends HTMLInputElement {
+class IdTable extends HTMLTableElement {
     constructor() {
         super();
-        this.type = "text";
-        this.maxLength = 1;
+        this.appendChild(document.createElement("tbody"));
+    }
+
+    static get observedAttributes() { return ["data-count"]; }
+
+    attributeChangedCallback(name, oldValue, newValue) {
+        if (name !== "data-count") return;
+
+        //let tableStr = "";
+        for (let i = 0; i < parseInt(newValue); i++) {
+            var tableRowEl, tableCellEl;
+            if (i % 32 == 0) {
+                tableRowEl = document.createElement("tr");
+                //tableStr += "<tr>";
+            }
+            tableCellEl = document.createElement("td");
+            tableCellEl.id = "can-id-"+i.toString();
+            tableCellEl.title = "0x"+i.toString(16).toUpperCase();
+            tableCellEl.textContent = i.toString();
+            tableRowEl.appendChild(tableCellEl);
+            //tableStr += `<td id="can-id-${i}" title="0x${i.toString(16).toUpperCase()}">${i}</td>`;
+            if (i % 32 == 32 - 1) {
+                this.firstElementChild.appendChild(tableRowEl);
+                //tableStr += "</tr>";
+            }
+        }
+        //this.firstElementChild.innerHTML = tableStr;
+        this.highlightIds();
+    }
+
+    highlightIds() {
+        const maskEl = document.getElementById("mask-input-list");
+        const filterEl = document.getElementById("filter-input-list");
+
+        const m = maskEl.getHexValue();
+        const f = filterEl.getHexValue();
+        for (let tr of this.firstElementChild.children) {
+            for (let td of tr.children) {
+                const n = parseInt(td.id.replace("can-id-", ""));
+                if ((n & m) == (f & m)) {
+                    td.classList.add("is-selected");
+                } else {
+                    td.classList.remove("is-selected");
+                }
+            }
+        }
     }
 }
 
 
 customElements.define('hex-input-list', HexInputList, { extends: 'ol' });
-customElements.define('hex-input', HexInput, { extends: 'input' });
+customElements.define('id-table', IdTable, { extends: 'table' });
--- script.js
+++ script.js
@@ -1,108 +1,16 @@
-var idTableEl, filterEl, maskEl;
-var currentCaret = 0;
-
-function createInputBoxes(id) {
-    //<input id="maskInput" class="input has-text-right has-text-grey is-size-1" type="text" pattern="0x[0-9a-fA-F]*" />
-}
-
-
 document.addEventListener("DOMContentLoaded", () => {
 
-    idTableEl = document.getElementById("idTable");
-    maskEl = document.getElementById("maskInput");
-    filterEl = document.getElementById("filterInput");
-
-    
-
-    maskEl.hexValue = 0;
-    maskEl.value = "0x0";
-    ["focus"].forEach(function (e) {
-        maskEl.addEventListener(e, checkcaret, false);
-        filterEl.addEventListener(e, checkcaret, false);
-    });
-    maskEl.addEventListener("input", (e)=>{
-        changeInput(e, maskEl);
-        highlightIds();
+    // Set bit length
+    document.querySelectorAll("input[name=bit-length]").forEach(radio => {
+        if (radio.checked) document.querySelectorAll('[is=hex-input-list]').forEach(el => el.dataset.count = Math.ceil(radio.dataset.value / 4));
     });
 
-    filterEl.hexValue = 0;
-    filterEl.value = "0x0";
-    filterEl.addEventListener("input", (e) => {
-        changeInput(e, filterEl);
-        highlightIds();
+    document.getElementById("bit-length-control").addEventListener("change", (e) => {
+        document.querySelectorAll('[is=hex-input-list]').forEach(el => el.dataset.count = Math.ceil(e.target.dataset.value/4));
     });
 
-    for (let el of document.querySelectorAll("input[name=bit-length]")) {
-        el.addEventListener("change", (e) => {
-            changeBitLength(parseInt(e.target.attributes["data-value"].value));
-        });
-    }
+    //createTable(512);
 
-    createTable(512);
-    changeBitLength(8);
+    document.getElementById("mask-input-list").children[2].firstElementChild.focus();
 
 });
-
-function changeBitLength(b) {
-    bitLength = b;
-    changeInput(null, maskEl);
-    changeInput(null, filterEl);
-    highlightIds();
-}
-
-function checkcaret(e) {
-    if (e.target.selectionStart < 2) setCaret(e.target, 2);
-}
-
-function setCaret(el, start) {
-    el.setSelectionRange(start, start);
-}
-
-function changeInput(e, el) {
-    currentCaret = el.selectionStart;
-    newVal = el.value.substr(1); 
-    newVal = newVal.replace("x", "");
-    if (e !== null && e.data && e.data.match(/[^0-9a-fA-F]/g)) {
-        currentCaret = currentCaret < 2 ? 2 : currentCaret;
-        currentCaret--;
-        newVal = newVal.replace(/[^0-9a-fA-F]/g, "");
-    } else if (e && !e.data) {
-        newVal = newVal.substr(0, currentCaret-2) + "0" + newVal.substr(currentCaret-2);
-    } else {
-        newVal = newVal.substr(0, currentCaret-2) + newVal.substr(currentCaret-1);
-    }
-    newVal = newVal.substr(0, bitLength);
-    el.value = "0x" + newVal.toUpperCase().padStart(bitLength, "0");
-    el.hexValue = parseInt(newVal, 16);
-    setCaret(el, currentCaret);
-}
-
-function highlightIds() {
-    m = maskEl.hexValue;
-    f = filterEl.hexValue;
-    for (let tr of idTableEl.children) {
-        for (let td of tr.children) {
-            n = parseInt(td.id.replace("can-id-",""));
-            if ((n & m) == (f & m)) {
-                td.classList.add("is-selected");
-            } else {
-                td.classList.remove("is-selected");
-            }
-        }
-    }
-}
-
-function createTable(n) {
-    tableStr = "";
-    for (let i = 0; i < n; i++) {
-        if (i % 32 == 0) {
-            tableStr += "<tr>";
-        }
-        tableStr += `<td id="can-id-${i}" title="0x${i.toString(16).toUpperCase()}">${i}</td>`;
-        if (i % 32 == 32 - 1) {
-            tableStr += "</tr>";
-        }
-    }
-    idTableEl.innerHTML = tableStr;
-    highlightIds();
-}
