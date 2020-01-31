d97176df2c1b88a0653f0aa58a298bf285948e21
Johan Winther <johan.winther@aptiv.com>
2020-01-31 18:00:01 +0100

    Update table range on refresh

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-    // Set bit length
-    document.querySelectorAll("input[name=bit-length]").forEach(radio => {
-        if (radio.checked) {
-            document.querySelectorAll('[is=hex-input-list]').forEach(el => el.dataset.count = Math.ceil(radio.dataset.value / 4));
-            document.querySelectorAll('[is=id-table]').forEach(el => el.dataset.count = Math.ceil(radio.dataset.value / 4));
-        }
-    });
-
+    // Set bit length upon change
     document.getElementById("bit-length-control").addEventListener("change", (e) => {
         document.querySelectorAll('[is=hex-input-list]').forEach(el => el.dataset.count = Math.ceil(e.target.dataset.value/4));
         document.querySelectorAll('[is=id-table]').forEach(el => el.dataset.count = Math.ceil(e.target.dataset.value / 4));
     });
 
+    // Trigger bit length event if refreshed
+    document.querySelectorAll("input[name=bit-length]").forEach(radio => {
+        if (radio.checked) radio.dispatchEvent(new Event("change", { bubbles: true }));
+    });
+
+    // Set range-min upon change
     document.getElementById('table-min').addEventListener("change", e => {
         if (e.target.value == "" || e.target.valueAsNumber < 0) e.target.value = "0";
         document.querySelectorAll('[is=id-table]').forEach(el => {
@@ -20,6 +19,7 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     });
 
+    // Set range-max upon change
     document.getElementById('table-max').addEventListener("change", e => {
         if (e.target.value == "" || e.target.valueAsNumber < 0) e.target.value = "0";
         document.querySelectorAll('[is=id-table]').forEach(el => {
@@ -27,6 +27,10 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     });
 
-    document.getElementById("mask-input-list").children[2].firstElementChild.focus();
+    // Trigger range change event if refreshed
+    [...document.getElementsByClassName('table-length-input')].forEach(el => el.dispatchEvent(new Event("change", { bubbles: true })));
+
+    // Set focus to first input
+    document.getElementById("mask-input-list").children[1].firstElementChild.focus();
 
 });
